9b783ed95e981d985d2697160649303ae3b7efda
jzcl9508 <sacremuh@gmail.com>
2026-05-11 20:41:20 +0200

    Conversion Solucionada

---
+++ Utils/DecimalInputConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+
+namespace SmartSaving.Utils
+{
+    public  class DecimalInputConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is decimal d && d == 0) return string.Empty;
+            return value?.ToString() ?? string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string s)
+            {
+                if (string.IsNullOrWhiteSpace(s)) return 0m;
+                s = s.Replace(",", ".");
+                if (decimal.TryParse(s, NumberStyles.Any, CultureInfo.InvariantCulture, out decimal result))
+                    return result;
+            }
+            return 0m;
+        }
+
+
+    }
+}
